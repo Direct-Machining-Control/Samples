c66178091692a176809d87c29c3c94a57dce7174
Tadas Kazakevicius <tadas.kaz@directmachining.com>
2025-10-28 08:13:14 +0200

    Added power meter example

---
+++ PM_Example/Device.cs
@@ -0,0 +1,125 @@
+﻿using Base;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM_Example
+{
+    /// <summary>
+    /// Example power meter device implementation.
+    /// </summary>
+    public class Device : Base.IPowerMeter
+    {
+        #region Local variables
+        private bool isConnected = false;
+        private double lastMeasured = 0;
+        private double averagingTime = 0;
+        private bool isMeasurementActive = false;
+        private ulong measurementID = 0;
+        private Task measurementTask = null;
+        private DeviceSettings deviceSettings;
+        #endregion
+
+        public Device(DeviceSettings deviceSettings)
+        {
+            this.deviceSettings = deviceSettings;
+        }
+
+        #region Base.IPowerMeter implementation
+        public bool IsConnected => isConnected;
+
+        public double LastMeasured => lastMeasured;
+
+        /// <summary>
+        /// Get new power measurement.
+        /// </summary>
+        public double GetPower 
+        { 
+            get 
+            {
+                // wait for new measurement
+                Stopwatch sw = new Stopwatch(); sw.Start();
+                var currentID = measurementID;
+                while (true)
+                {
+                    if (!isMeasurementActive || !isConnected) return -1;
+                    if (measurementID > currentID) break;
+                    System.Threading.Thread.Sleep(10);
+                    if (sw.Elapsed.TotalSeconds > 5) return -1; // timeout
+                }
+
+                return lastMeasured; 
+            }
+        }
+
+        public double AveragingTime { get => averagingTime; set => averagingTime = value; }
+
+        public bool Connect()
+        {
+            Disconnect();
+            isConnected = true;
+            measurementID = 0;
+            return isConnected;
+        }
+
+        public void Disconnect()
+        {
+            Stop();
+            isConnected = false;
+        }
+
+        public string GetErrorMessage()
+        {
+            return Base.Functions.GetLastErrorMessage();
+        }
+
+        public string GetName()
+        {
+            return "Power Meter Example";
+        }
+
+        public bool SetWavelength(double wavelength)
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Start continuous power measurement.
+        /// </summary>
+        /// <returns>FALSE if error</returns>
+        public bool Start()
+        {
+            Stop();
+            measurementTask = Task.Run(() =>
+            {
+                // simulate power measurement
+                Stopwatch sw = new Stopwatch(); sw.Start();
+                while (isConnected && isMeasurementActive)
+                {
+                    lastMeasured = Base.Geometry.Mod(sw.Elapsed.TotalSeconds, deviceSettings.MaxPowerToShow); // Simulate power measurement between 10 and 100
+                    measurementID++;
+                    System.Threading.Thread.Sleep(100); // Simulate a delay for measurement
+                }
+            });
+            isMeasurementActive = true;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Stop continuous power measurement.
+        /// </summary>
+        public void Stop()
+        {
+            isMeasurementActive = false;
+            measurementTask?.Wait();
+        }
+
+
+        #endregion
+    }
+}
---
+++ PM_Example/DeviceSettings.cs
@@ -0,0 +1,41 @@
+﻿using Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM_Example
+{
+    /// <summary>
+    /// Example power meter device settings implementation.
+    /// </summary>
+    public class DeviceSettings : Base.IPowerMeterSettings
+    {
+        Device power_meter = null;
+
+        DoubleExtParameter max_power_to_show = new DoubleExtParameter(nameof(max_power_to_show), "Max Power to Show (W)", "What max power should be shown", 1, 0.01, 1000, 0.01);
+
+        /// <summary>
+        /// Maximum power to show for device (W)
+        /// </summary>
+        public double MaxPowerToShow { get { return max_power_to_show.value; } }
+
+        public DeviceSettings() : base("pm_example", "Power Meter Example", "Simulates power")
+        {
+            Parameters.Clear();
+            Add(max_power_to_show);
+        }
+
+        public override bool CanPerformAutomaticDetection()
+        {
+            return false;
+        }
+
+        public override IPowerMeter GetPowerMeter()
+        {
+            if (power_meter != null) return power_meter;
+            return power_meter = new Device(this);
+        }
+    }
+}
